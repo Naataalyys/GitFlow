50a5bdc367a651dcc250c9d007dd998d89832c33
naata <bocharova.nata@bk.ru>
2023-06-04 13:50:06 +0700

    add new function

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def new_word(word = input().upper()):
+  """Функция, принимающая на вход строку и возвращающая ее с заглавными буквами"""
   return word
 
 print(new_word())
+
+def new_text(text = input().title()):
+  """Функция, которая делает заглавными первые буквы каждого слова, поступившего на вход"""
+
+  return text
+
+  print(new_text())
